6cbde3674d9c38698f066109c6153e6290a42520
Sashidhar Joganna <sashizen@gmail.com>
2022-12-06 03:01:23 +0100

    base project for ui and api tests

---
+++ BDD_KnabUIAndAPI/Features/TrelloLogin.feature
@@ -0,0 +1,12 @@
+﻿Feature: TrelloLogin
+	Land on the Trello home page and click on sign in button
+
+@mytag
+Scenario: User logs in using his username and password
+Given the user has a valid login credentials
+When the user clicks on the "Login" button
+And the user enters "Username" and "Password"
+| Username            | Password |
+| l0gint3st@gmail.com | Testen01 |
+And the user clicks on the "LoginSubmit" button 
+Then the user is successfully logged in to dashboard "QA Automation"
---
+++ BDD_KnabUIAndAPI/Steps/TrelloLoginStepDefinitions.cs
@@ -0,0 +1,79 @@
+﻿using BDD_KnabUIAndAPI.Components;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace BDD_KnabUIAndAPI
+{
+    [Binding]
+    public sealed class TrelloLoginStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        public static IWebDriver Driver {get; set;}
+        public static int Timeout = 30;
+
+        [BeforeScenario]
+
+        public void BeforeScenario()
+        {
+          
+            CommonSteps.InitializeTest();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            CommonSteps.EndTest();
+        }
+
+        [Given(@"the user has a valid login credentials")]
+        public void GivenTheUserHasAValidLoginCredentials()
+        {
+            
+        }
+
+        [When(@"the user clicks on the login button")]
+        public void WhenTheUserClicksOnTheLoginButton(String t)
+        {
+            Assert.Equal(t, LoginLocators.LoginButton.Text);
+            LoginLocators.ClickEvent();
+        }
+
+        [When(@"the user enters ""(.*)"" and ""(.*)""")]
+        public void WhenTheUserEntersAnd(string userName, string password)
+        {
+            LoginLocators.UserNameInput.SendKeys(userName);
+            LoginLocators.ContinueButton.Click();
+            Assert.Equal(userName, LoginLocators.LoggingUser.Text);
+            LoginLocators.PasswordInput.SendKeys(password);
+                    }
+
+        [When(@"the user clicks on the ""(.*)"" button")]
+        public void WhenTheUserClicksOnTheButton(string Submit)
+        {
+
+            Assert.Equal(Submit, LoginLocators.LoginSubmitButton.Text);
+            LoginLocators.LoginSubmitButton.Click();
+           
+        }
+
+        [Then(@"the user is successfully logged in to dashboard ""(.*)""")]
+        public void ThenTheUserIsSuccessfullyLoggedInToDashboard(string p0)
+        {
+            Console.WriteLine("User logged in");        
+        }
+
+
+
+
+
+    }
+}
